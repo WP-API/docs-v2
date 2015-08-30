c4044465a735d92c2c4e491169b7eaf0234483bf
Rachel Baker <rachel@rachelbaker.me>
2015-08-30 18:51:52 -0500

    Add terms endpoint to schema regeneration script

--- regenerate.rb
+++ regenerate.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'json'
 
-schemas = [ 'comments', 'media', 'posts', 'pages', 'statuses', 'taxonomies', 'types', 'users']
+schemas = [ 'comments', 'media', 'posts', 'pages', 'statuses', 'taxonomies', 'terms','types', 'users']
 
 res = Net::HTTP.start("demo.wp-api.org", 80) {
   |http|
